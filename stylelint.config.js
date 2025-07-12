3bc45787350dc2907ada8af91c9bc0b48daa7bbf
agolovachev <antongolova@gmail.com>
2025-07-12 15:55:14 +0300

    feat: добавлены зависимости для стилей и конфигурация Stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen'
+        ]
+      }
+    ],
+    'function-no-unknown': [
+      true,
+      {
+        ignoreFunctions: ['theme']
+      }
+    ]
+  }
+} 
